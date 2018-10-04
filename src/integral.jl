2e4c1a808d7ecaa60a9fcd6d41c69b13868d3493
Josua Grawitter <josua.grawitter@campus.tu-berlin.de>
2018-10-04 12:31:11 +0200

    fix 0.7 depracations and add Pkg3 files

--- src/integral.jl
+++ src/integral.jl
@@ -15,13 +15,13 @@ _H0_integral(z::Real) = _K0_integral(z) + bessely0(z)
 _H_integral(ν, z::Real) = _K_integral(ν, z) + bessely(ν, z)
 
 # only valid for real(ν) > -0.5, https://dlmf.nist.gov/11.5.E1
-_H0_integral{T <: Real}(z::Complex{T}) = (2 / pi) *
+_H0_integral(z::Complex{T}) where {T <: Real} = (2 / pi) *
     integrate(ϑ -> sin(z * cos(ϑ)), 0, 0.5pi)
-_H_integral{T <: Real}(ν, z::Complex{T}) = 2(0.5z)^ν /
+_H_integral(ν, z::Complex{T}) where {T <: Real} = 2(0.5z)^ν /
     (sqrt(pi) * gamma(ν + 0.5)) *
     integrate(t -> (1 - t^2)^(ν - 0.5) * sin(z * t), 0, 1)
-_K0_integral{T <: Real}(z::Complex{T}) = _H0_integral(z) - bessely0(z)
-_K_integral{T <: Real}(ν, z::Complex{T}) = _H_integral(ν, z) - bessely(ν, z)
+_K0_integral(z::Complex{T}) where {T <: Real} = _H0_integral(z) - bessely0(z)
+_K_integral(ν, z::Complex{T}) where {T <: Real} = _H_integral(ν, z) - bessely(ν, z)
 
 # only valid for real(ν) > -0.5, https://dlmf.nist.gov/11.5.E4
 _M0_integral(z) = -(2 / pi) * integrate(ϑ -> exp(-z * cos(ϑ)), 0, 0.5pi)
