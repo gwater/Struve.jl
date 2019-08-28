4ac38eefe64e374d30347008e00d29221cef28ec
Josua Grawitter <josua.grawitter@campus.tu-berlin.de>
2019-08-28 14:55:46 +0200

    implement very simple test for validation

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,10 @@
 using Struve
-using Base.Test
+using Test
 
-# write your own tests here
-@test 1 == 2
+# using Table 2 from
+# https://doi.org/10.1016/S0377-0427(01)00580-5
+@testset "H0 zeros" begin
+    @test abs(Struve.H0(22.9490276305)) < 1e-10
+    @test !(abs(Struve.H0(22.0)) < 1e-10)
+end
+
