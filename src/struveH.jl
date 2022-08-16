fb9df020f3d60aa1b43d01f6fd5c7b1ce1b016f9
Michael Helton <heltonmc@protonmail.com>
2022-08-16 13:29:35 -0400

    default to integral

--- src/struveH.jl
+++ src/struveH.jl
@@ -1,3 +1,24 @@
+function struveH(v::Real, x::T) where T
+    if struveK_large_arg_cutoff(v, x)
+        # use http://dlmf.nist.gov/11.2.E6
+        return struveK_large_argument(v, x) + bessely(v, x)
+    elseif struveH_power_series_cutoff(v, x)
+        return struveH_power_series(v, x)
+    else
+        return _H_integral(v, x)
+    end
+end
+
+function struveM(v::Real, x::T) where T
+    if struveK_large_arg_cutoff(v, x)
+        return struvek_large_argument(v, x)
+    elseif struveH_power_series_cutoff(v, x)
+        return struveH_power_series(v, x) - bessely(v, x)
+    else
+        return _K_integral(v, x)
+    end
+end
+
 # H_{nu}(x) Struve function power series
 # http://dlmf.nist.gov/11.2.E1
 # can use for x < 5 || nu > -0.75 + 0.41x + 0.023x^2
@@ -7,7 +28,7 @@
 function struveH_power_series(v, x::T) where T
     MaxIter = 50000
     out = zero(T)
-    a = (x/2)^(v+1) / (gamma(v + T(3)/2) * GAMMA_THREE_HALF(T))
+    a = (x/2)^(v+1) / (gamma(v + T(3)/2) * gamma(T(3)/2))
     iszero(a) && return a
     t2 = (x/2)^2
     for i in 0:MaxIter
@@ -18,10 +39,6 @@ function struveH_power_series(v, x::T) where T
     return out
 end
 struveH_power_series_cutoff(nu, x) = x < 5 || nu > evalpoly(x, (-0.75, 0.41, 0.023))
-
-const GAMMA_THREE_HALF(::Type{Float32}) = 0.886226925452758f0
-const GAMMA_THREE_HALF(::Type{Float64}) = 0.886226925452758
-#const GAMMA_THREE_HALF(::Type{BigFloat}) = big"0.8862269254527580136490837416705725913987747280611935641069038949264556422955125"
 
 # K_{nu}(x) using large argument expansion
 # http://dlmf.nist.gov/11.6.E1
@@ -38,3 +55,4 @@ function struveK_large_argument(v, x::T) where T
     end
     return out / T(pi)
 end
+struveK_large_arg_cutoff(nu, x) = x > 35 && nu < 1.6*x - 39.0
