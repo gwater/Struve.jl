2389580522520ddeb8bd5a33fe72c4f0b0238e90
Michael Helton <heltonmc@protonmail.com>
2022-08-24 10:55:11 -0400

    add overflow checks and logs

--- src/struvel.jl
+++ src/struvel.jl
@@ -58,20 +58,38 @@ end
 # L_{nu}(x) Modified Struve function power series
 # http://dlmf.nist.gov/11.2.E2
 # power series can always be applied without significant cancellation
-# could be premature overflow for large orders
-# TODO: use either large expansion or use log power series
+# For large orders the logarithm of the power series is used
+# to avoid overlow in gamma functions
 function struvel_power_series(v, x::T) where T
     MaxIter = 10000
     out = zero(T)
-    a = (x/2)^(v+1) / (gamma(v + T(3)/2) * gamma(T(3)/2))
+    three_halves = T(3) / 2
+
+    a = (x/2)^(v+1)
+    (isinf(a) || gamma_max(v)) && return log_struvel_power_series(v, x)
+    a /= gamma(v + three_halves) * gamma(three_halves)
     iszero(a) && return a
+
     t2 = (x/2)^2
     for i in 0:MaxIter
         out += a
         abs(a) < eps(T) * abs(out) && break
-        a *= inv((v + i + 3/2) * (i + 3/2)) * t2
+        a *= inv((v + i + three_halves) * (i + three_halves)) * t2
     end
     return out
+end
+function log_struvel_power_series(v, x::T) where T
+    MaxIter = 10000
+    out = zero(T)
+    three_halves = T(3) / 2
+    a = x / 2 
+    t2 = a*a
+    for i in 0:MaxIter
+        out += a
+        abs(a) < eps(T) * abs(out) && break
+        a *= inv((v + i + three_halves) * (i + three_halves)) * t2
+    end
+    return exp(v*log(x/2) + log(out) - loggamma(v + three_halves) - loggamma(three_halves))
 end
 
 # M_{nu}(x) using large argument expansion
@@ -81,16 +99,38 @@ function struvem_large_argument(v, x::T) where T
     MaxIter = 50000
     out = zero(T)
     one_half = one(T) / 2
-    a = (x/2)^(v-1) * gamma(one_half) / gamma(v + one_half)
+
+    a = (x/2)^(v-1)
+    (isinf(a) || gamma_max(v)) && return log_struvem_large_argument(v, x)
+    a *= gamma(one_half) / gamma(v + one_half)
     iszero(a) && return a
+
     t2 = (x/2)^(-2)
     for i in 0:MaxIter
         out += a
         abs(a) < eps(T) * abs(out) && break
         a *= -(i + one_half) * t2 * (v - one_half - i)
     end
-    return out / T(pi)
+    return out / π
+end
+
+function log_struvem_large_argument(v, x::T) where T
+    MaxIter = 50000
+    out = zero(T)
+    one_half = one(T) / 2
+    a = 2 / x
+    iszero(a) && return a
+    t2 = a*a
+    for i in 0:MaxIter
+        out += a
+        abs(a) < eps(T) * abs(out) && break
+        a *= -(i + one_half) * t2 * (v - one_half - i)
+    end
+    return exp(log(out / π) + loggamma(one_half) - loggamma(v + one_half) + v*log(x/2)) 
 end
 
 struvem_large_arg_cutoff(nu, x::Float64)  = x > 35 && nu < evalpoly(x, (7.0, -0.59, 0.011))
 struvem_large_arg_cutoff(nu, x::Float32)  = x > 32 && nu < 1.5f0*x
+
+gamma_max(x::Float64) = x > 160
+gamma_max(x::Float32) = x > 32
